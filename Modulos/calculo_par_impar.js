c1363fad49d86970666eddcbf95c3ccadf918758
GYOVANNE MARTINS SABARÁ <22102360@dsn127m1242376.cfp127aluno.local>
2022-08-15 08:59:00 -0300

    Update

--- Modulos/calculo_par_impar.js
+++ Modulos/calculo_par_impar.js
@@ -13,7 +13,7 @@ const calculo = function(valorInicial, valorFinal){
     console.log('Lista de números Pares\n');
     for(let i = inicial; i <= final; i++){
         resultado = i%2;
-        if(resultado == 0){
+        if(resultado == 0){ 
             console.log(i + '\n')
         }
     }
@@ -51,12 +51,69 @@ const par = function(valorInicial, valorFinal){
     for(let i = inicial; i <= final; i++){
         resultado = i%2;
         if(resultado == 0){
-            console.log(i + '\n')
+            console.log(i + '\n');
+
         }
     }
     process.exit(0);
 }
 
+const escolhas = function(escolha, primeiro, ultimo){
+    let escolhe = escolha; 
+    let inicial = primeiro;
+    let final = ultimo;
+
+    if(escolhe == ''){
+        console.log('Por favor digte algo');
+    } if(escolhe == 'ambos' || escolhe == 'os dois'){
+        calculo(inicial, final);
+    } else if(escolhe == 'pares' || escolhe == 'par'){
+        par(inicial, final);
+    } else if(escolhe == 'impares' || escolhe == 'impar'){
+        impar(inicial, final);
+    } else{
+        console.log('Escreveste algo de errado.');
+    }
+
+    process.exit(0);
+}
+
+const inicialCerto = function(valorInicial){
+    let inicial = valorInicial;
+
+    if(inicial == ''){
+        console.log('Por favor digite o número inicial!');
+        process.exit(0);
+    } 
+    if(inicial < 0 || inicial > 500){
+        console.log('Por favor, apena um número entre 0 e 500');
+        process.exit(0);
+    } 
+}
+
+const finalCerto = function(valorFinal, valorInicial){
+    let final = valorFinal;
+    let inicial = valorInicial;
+
+    if(final == ''){
+        console.log('Por favor digite o número final!');
+        process.exit(0);
+    } 
+    if(final < 100 || final > 1000){
+        console.log('Por favor, apena um número entre 100 e 1000');
+        process.exit(0);
+    } 
+    if(final < inicial){
+        console.log(final + inicial)
+        console.log('Por favor, o final deve ser maior que o inicial!');
+        process.exit(0);
+    } 
+    if(final == inicial){
+        console.log('Por favor, devem ser diferentes');
+        process.exit(0);
+    } 
+}
+
 module.exports = {
-    calculo, par, impar
+    calculo, par, impar, escolhas, inicialCerto, finalCerto
 }
--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 console.log('Calculo de par e impar');
 
 //import do arquivo de função para realizar calculos
-const {calculo, par, impar} = require('./Modulos/calculo_par_impar.js');
+const {calculo, par, impar, escolhas, inicialCerto, finalCerto} = require('./Modulos/calculo_par_impar.js');
 var readLine = require('readline');
 
 var entradaDados = readLine.createInterface({
@@ -17,54 +17,19 @@ var entradaDados = readLine.createInterface({
 });
 
 entradaDados.question('Escolha o começo (de 0 - 500): ', function (valorInicial){
-    let inicial = valorInicial;
+    let inicial = parseInt(valorInicial);
 
-    if(inicial == ''){
-        console.log('Por favor digite o número inicial!');
-        process.exit(0);
-    } 
-    if(inicial < 0 || inicial > 500){
-        console.log('Por favor, apena um número entre 0 e 500');
-        process.exit(0);
-    } 
+    inicialCerto(inicial);
+
     entradaDados.question('Escolha o final (de 100 - 1000): ', function (valorFinal){
-        let final = valorFinal;
-    
-        if(final == ''){
-            console.log('Por favor digite o número final!');
-            process.exit(0);
-        } 
-        if(final < 100 || final > 1000){
-            console.log('Por favor, apena um número entre 100 e 1000');
-            process.exit(0);
-        } 
-        if(final < inicial){
-            console.log('Por favor, o final deve ser maior que o inicial!');
-            process.exit(0);
-        } 
-        if(final == inicial){
-            console.log('Por favor, devem ser diferentes');
-            process.exit(0);
-        } 
+        let final = parseInt(valorFinal);
+
+        finalCerto(final, inicial);
 
         entradaDados.question('Do que precisas? Mostrar pares, impares ou ambos: ', function (escolhaDado){
-            let escolha = sescolhaDado;
+            let escolha = escolhaDado;
             
-            if(escolha == ''){
-                console.log('Por favor digte algo');
-                process.exit(0);
-            } if(escolha == 'ambos'){
-                calculo(inicial, final);
-                process.exit(0);
-            } else if(escolha == 'pares'){
-                par(inicial, final);
-                process.exit(0);
-            } else if(escolha == 'impares'){
-                impar(inicial, final);
-                process.exit(0);
-            } else{
-                console.log('Escreveste algo de errados')
-            }
+            escolhas(escolha, inicial, final);
         });
     });
 });
